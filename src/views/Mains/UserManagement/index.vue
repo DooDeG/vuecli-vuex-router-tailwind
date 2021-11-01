c601068e5bfeec156423480c20201de5758afa20
timtimoffice <timoffice8@gmail.com>
2021-11-01 14:46:58 +0800

    update

--- src/views/Mains/UserManagement/index.vue
+++ src/views/Mains/UserManagement/index.vue
@@ -1,50 +1,125 @@
-
 <template>
-    <div class="UserIndex">
-        <div class="text-gray-900 bg-gray-200">
-            <div class="p-4 flex">
-                <h1 class="text-3xl">
-                    Users
-                </h1>
-            </div>
-            <div class="px-3 py-4 flex justify-center">
-                <table class="w-full text-md bg-white shadow-md rounded mb-4">
-                    <tbody>
-                        <tr class="border-b">
-                            <th class="text-left p-3 px-5">Name</th>
-                            <th class="text-left p-3 px-5">Email</th>
-                            <th class="text-left p-3 px-5">Role</th>
-                            <th></th>
-                        </tr>
-                        <tr class="border-b hover:bg-orange-100 bg-gray-100">
-                            <td class="p-3 px-5"><input type="text" value="user.name" class="bg-transparent"></td>
-                            <td class="p-3 px-5"><input type="text" value="user.email" class="bg-transparent"></td>
-                            <td class="p-3 px-5">
-                                <select value="user.role" class="bg-transparent">
-                                    <option value="user">user</option>
-                                    <option value="admin">admin</option>
-                                </select>
-                            </td>
-                            <td class="p-3 px-5 flex justify-end"><button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Update</button><button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button></td>
-                        </tr>
-                       
-                    </tbody>
-                </table>
-            </div>
+    <div id="UserIndex">
+        
+        <div class="flex-1 flex flex-col overflow-hidden">
+            
+            <main class="flex-1 bg-gray-200">
+                <div class="container mx-auto px-6 py-8">
+                    <h3 class="text-gray-700 text-3xl font-medium">Dashboard</h3>
+    
+    
+                    <div class="mt-8">
+                        <div class="flex flex-col mt-8">
+                            <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                                <div
+                                    class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                                    <table class="min-w-full">
+                                        <thead>
+                                            <tr class="">
+                                                <th
+                                                    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                Name</th>
+                                                <th
+                                                    class=" px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                    <div class="flex justify-start">Email</div>
+                                                </th>
+                                                <th
+                                                    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                                    <div class="flex justify-start">Role</div>
+                                                </th>
+                                                
+                                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+                                            </tr>
+                                        </thead>
+        
+                                        <tbody class="bg-white">
+                                            <tr>
+                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                    <div class="flex items-center">
+                                                        <div class="flex-shrink-0 h-10 w-10">
+                                                            <img class="h-10 w-10 rounded-full"
+                                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+                                                                alt="">
+                                                        </div>
+        
+                                                        <div class="ml-4">
+                                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe
+                                                            </div>
+                                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                                                        </div>
+                                                    </div>
+                                                </td>
+        
+                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                    <div class="flex justify-start">
+                                                        <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
+                                                        
+                                                    </div>
+                                                </td>
+        
+                                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                                    <div class="flex justify-start">
+                                                    <span
+                                                        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+                                                    </div>
+                                                </td>
+        
+                                                <td
+                                                    class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                                                        <div class="flex item-center justify-center">
+                                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                                            </svg>
+                                                        </div>
+                                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                            </svg>
+                                                        </div>
+                                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                            </svg>
+                                                        </div>
+                                                    </div>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        
+                                        
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    
+                </div>
+            </main>
         </div>
+
+        <deleteBox v-show="del"/>
     </div>
 </template>
 
 <script>
 import axios from "axios";
-
+import deleteBox from '@/components/deleteBox.vue';
+// @ is an alias to /src
 export default {
     name: 'UserIndex',
+    components:{
+        deleteBox
+    },
     data() {
         return {
+            usersList: [],
+            del:false
         };
-    },
-    components: {
     },
     mounted() {
         axios.get("https://jsonplaceholder.typicode.com/users")
@@ -54,14 +129,30 @@ export default {
         })
         .catch(error => {
             console.log(error)
+            // Manage errors if found any
         })
-    },
-    methods:{
-
     }
-}
+    }
 </script>
-
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
