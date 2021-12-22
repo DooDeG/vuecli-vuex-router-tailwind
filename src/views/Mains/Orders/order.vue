6359a864d9eabcaf5d78fad2e8370febbe61e759
unknown <doodegre@gmail.com>
2021-12-23 00:13:03 +0800

    update dist

--- src/views/Mains/Orders/order.vue
+++ src/views/Mains/Orders/order.vue
@@ -77,6 +77,7 @@
                                         <div>
                                             <p class="mb-2 text-sm font-medium text-gray-600">
                                                 總會員數
+                                                
                                             </p>
                                             <p class="text-lg font-semibold text-gray-700">
                                                 {{account}}
