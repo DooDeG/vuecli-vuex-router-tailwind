94dd0b3aa808a08692503658b28037c6f40e49e8
unknown <doodegre@gmail.com>
2021-12-23 00:08:38 +0800

    order record

--- src/views/Mains/Orders/order.vue
+++ src/views/Mains/Orders/order.vue
@@ -11,6 +11,81 @@
                         </div>
                     </nav>
                     <div class="mt-8">
+                         <div class="mt-10">
+                            <div class="container px-6 mx-auto grid">
+                                <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+                                    <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                                        <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full">
+                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                                                <path
+                                                    d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
+                                                </path>
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <p class="mb-2 text-sm font-medium text-gray-600">
+                                                未處理訂單
+                                            </p>
+                                            <p class="text-lg font-semibold text-gray-700">
+                                                {{countPending}}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                                        <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full">
+                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                                                <path
+                                                    d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
+                                                </path>
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <p class="mb-2 text-sm font-medium text-gray-600">
+                                                今日訂單量
+                                            </p>
+                                            <p class="text-lg font-semibold text-gray-700">
+                                                {{countOrder}}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                                        <div class="p-3 mr-4 text-red-500 bg-red-100 rounded-full">
+                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                                                <path
+                                                    d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
+                                                </path>
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <p class="mb-2 text-sm font-medium text-gray-600">
+                                                今日營業額
+                                            </p>
+                                            <p class="text-lg font-semibold text-gray-700">
+                                                {{countTodayMoney}} MOP
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                                        <div
+                                            class="p-3 mr-4 text-yellow-500 bg-yellow-100 rounded-full">
+                                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                <circle cx="12" cy="8" r="7"></circle>
+                                                <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <p class="mb-2 text-sm font-medium text-gray-600">
+                                                總會員數
+                                            </p>
+                                            <p class="text-lg font-semibold text-gray-700">
+                                                {{account}}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div class="w-full flex justify-between">
                             <div class="flex bg-white border rounded-full overflow-hidden select-none">
                                 <button @click="ratioSelect" class=" hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50
@@ -145,7 +220,11 @@ export default {
             sidebarOpen: false,
             slug:'',
             type: [],
-            radioP:'today'
+            radioP:'today',
+            countPending: 0,
+            countOrder: 0,
+            countTodayMoney: 0,
+            account:0
         };
     },
     components: {
@@ -154,9 +233,52 @@ export default {
     },
     created(){
         // 
-        this.getTodayOrder();
+        // this.getTodayOrderPending();
+        this.init()
     },
     methods:{
+        init(){
+            // this.getAllOrder();
+            this.countOrder = 0
+            axios.get(this.$baseUrl+'Order/getAll')
+                .then(res => {
+                    res.data.value.order.forEach(item => {
+                        this.countOrder ++
+                    });
+                })
+                .catch(error => {
+                    console.log(error)
+                    // Manage errors if found any
+            })
+            // this.getTodayOrderPending();
+            this.order = [];
+            this.countPending = 0
+            axios.get(this.$baseUrl+'Order/GetOrderByTodayPending')
+                .then(res => {
+                    res.data.value.order.forEach(item => {
+                        this.order.push(item) 
+                        this.countPending ++
+                    });
+                })
+                .catch(error => {
+                    console.log(error)
+                    // Manage errors if found any
+            })
+
+            // this.getAllTodayOrder()
+            this.countTodayMoney = 0
+            axios.get(this.$baseUrl+'Order/getAllTodayOrder')
+                .then(res => {
+                    res.data.value.order.forEach(item => {
+                        this.countTodayMoney += item.price
+                    });
+                })
+                .catch(error => {
+                    console.log(error)
+                    // Manage errors if found any
+            })
+
+        },
         ratioSelect(){
             if(this.radioP == "all"){
                 this.getAllOrder() 
@@ -165,7 +287,7 @@ export default {
             }else if(this.radioP == "allCancelOrder"){
                 this.getTodayOrderCancel()
             }else{
-                this.getTodayOrder()
+                this.getTodayOrderPending()
             }
 
             
@@ -183,12 +305,14 @@ export default {
                     // Manage errors if found any
             })
         },
-        getTodayOrder() {
+        getTodayOrderPending() {
             this.order = [];
+            this.countPending = 0
             axios.get(this.$baseUrl+'Order/GetOrderByTodayPending')
                 .then(res => {
                     res.data.value.order.forEach(item => {
                         this.order.push(item) 
+                        this.countPending ++
                     });
                 })
                 .catch(error => {
@@ -212,10 +336,12 @@ export default {
         
         getAllOrder() {
             this.order = [];
+            this.countOrder = 0
             axios.get(this.$baseUrl+'Order/getAll')
                 .then(res => {
                     res.data.value.order.forEach(item => {
                         this.order.push(item) 
+                        this.countOrder ++
                     });
                 })
                 .catch(error => {
